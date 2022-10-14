42486eedc0478f85323acba99a0135e20924bb23
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-10-14 16:51:01 -0400

    working on displaying the spinner. added setInterval function to the saveButton function which works but doesnt show the spinner

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const saveButton = (e) => {
+  // when clicking the button:
+      // change the background of the button to red
+        // button.style.backgroundColor = 'red';
+        // button.style.color = '#fff';
+
+      button.classList.toggle('liked')
+      // hide the hollow heart
+      const hollowIcon = document.querySelector('.fa-regular')
+      const solidIcon = document.querySelector('.solidHeart')
+      const loadingIcon = document.querySelector('.fa-spinner-third')
+      const save = document.querySelector('.save')
+      const saved = document.querySelector('.saved')
+
+      hollowIcon.classList.toggle('hidden')
+      save.classList.toggle('hidden')
+      saved.classList.toggle('hidden')
+      // solidIcon.classList.toggle('hidden')
+      // show the loading icon for a certain amount of time
+        setInterval(() => { 
+          loadingIcon.classList.remove('hidden')
+          console.log('set interval triggered')
+        }, 1000)
+      // show the solid heart
+      // solidIcon.classList.toggle('hidden')
 }
 
-document.addEventListener('DOMContentLoaded', main)
+
+const button = document.querySelector('button')
+button.addEventListener('click', saveButton)
+
